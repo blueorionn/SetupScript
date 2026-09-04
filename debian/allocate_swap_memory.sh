c84ff253d3d29a7e7260e4b6220470411383da86
Swadhin <168361244+blueorionn@users.noreply.github.com>
2026-09-04 11:55:13 +0530

    fix: prompt allocation memory size

--- debian/allocate_swap_memory.sh
+++ debian/allocate_swap_memory.sh
@@ -2,7 +2,7 @@
 # Copyright (c) 2025-present, Swadhin
 
 # A swap file acts as virtual memory, allowing your system to use disk storage when RAM is full. 
-# E.g. bash allocate_swap_memory.sh MEMORY-IN-GB
+# E.g. bash allocate_swap_memory.sh
 
 # Check if the script is running with sudo (root privileges)
 if [[ $EUID -ne 0 ]]; then
@@ -34,31 +34,23 @@ fi
 echo "[INFO] $(date +"%Y-%m-%d %H:%M:%S") - Checking for existing swap file."
 sudo swapon --show
 
-# Getting memory size and checking it's validity
-if [ -z "$1" ]; then
-  echo "[ERROR] $(date +"%Y-%m-%d %H:%M:%S") - No memory size provided."
-  exit 1
-fi
-
-if ! [[ "$1" =~ ^[0-9]+$ ]]; then
-  echo "[ERROR] $(date +"%Y-%m-%d %H:%M:%S") - Argument is not a valid number."
-  exit 1
-fi
-
-if [ "$1" -le 0 ]; then
-  echo "[ERROR] $(date +"%Y-%m-%d %H:%M:%S") - Argument must be greater than zero."
-  exit 1
-fi
+# Prompt for swap size and validate it
+while true; do
+    read -rp "[INPUT] Enter swap size in MB: " SWAP_SIZE_MB
+    if [[ "$SWAP_SIZE_MB" =~ ^[0-9]+$ ]] && [[ "10#$SWAP_SIZE_MB" -gt 0 ]]; then
+        break
+    fi
+    echo "[ERROR] $(date +"%Y-%m-%d %H:%M:%S") - Invalid input. Enter digits only (greater than zero)."
+done
 
 # Creating swapfile
 PARENT_FOLDER="/swaps"
 FILE_NAME="swapfile"
 FILE_PATH="/swaps/$FILE_NAME"
-FILE_SIZE=$1
 
-echo "[INFO] $(date +"%Y-%m-%d %H:%M:%S") - Creating $FILE_NAME in path $FILE_PATH"
+echo "[INFO] $(date +"%Y-%m-%d %H:%M:%S") - Creating $FILE_NAME of size ${SWAP_SIZE_MB}MB in path $FILE_PATH"
 sudo mkdir -p "$PARENT_FOLDER"
-sudo fallocate -l "$FILE_SIZE"G "$FILE_PATH"
+sudo fallocate -l "${SWAP_SIZE_MB}M" "$FILE_PATH"
 
 # Set Correct Permissions
 echo "[INFO] $(date +"%Y-%m-%d %H:%M:%S") - Setting correct permissions"
